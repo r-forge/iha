da799e7c77f4f1f12289b8a5bf0952e5ba7b2b76
jaylaw (Jason Law) <jason.e.law@gmail.com>
2010-09-16 23:53:56 +0000

    Rewrote group2 function: removed extrema and annual.extreme functions.  group2 now uses plyr package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/iha@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/group2.R
+++ pkg/R/group2.R
@@ -1,17 +1,5 @@
-`group2` <-
-function (x, yr = NULL) 
-{
-    if (is.null(yr)) 
-        yr <- water.year(index(x))
-    ext <- annual.extrema(x)
-    sx <- split(x, yr)
-    zeros <- sapply(sx, function(x) length(which(x == 0)))
-    baseindex <- sapply(sx, function(x) min(rollmean(x, 7))/mean(x))
-    cbind(ext, "Zero flow days" = zeros, "Base index" = baseindex)
-}
-
-'group2.alt' <-
-function(x, year, window = c(1, 3, 7, 30, 90)){
+'group2' <-
+function(x, year, window = c(1, 3, 7, 30, 90), ...){
 	rollx <- mapply('runmean', k = window, MoreArgs = list(x = coredata(x), alg = 'fast', endrule = 'NA'))
 	xd <- data.frame(year = as.factor(water.year(index(x))))
 	xd$val <- rollx
@@ -21,7 +9,7 @@ function(x, year, window = c(1, 3, 7, 30, 90)){
 				baseindex <- min(x$val[,3], na.rm=T) / mean(x$val[,1])
 				zeros <- length(which(x[,1] == 0))
 				c(rng, zeros, baseindex)
-			})
+			}, ...)
 	nms <- sprintf(c('%1$s Day Min', '%1$s Day Max'), rep(window, each=2))
 	names(res) <- c('Year', nms, 'Zero flow days', 'Base index')
 	return(res)
