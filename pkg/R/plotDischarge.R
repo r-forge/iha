8e94d7a4f2faa00de9528ba3e42f6aaadd8dc6a2
jaylaw (Jason Law) <jason.e.law@gmail.com>
2011-12-15 20:42:30 +0000

    fix plot bug and group1 function bug
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/iha@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plotDischarge.R
+++ pkg/R/plotDischarge.R
@@ -1,19 +1,17 @@
 `plotDischarge` <-
-function (z, palette, ...) 
+function (z, palette = 'Dark2', log = F, ...) 
 {
-    require(lattice)
-    require(RColorBrewer)
     y <- as.numeric(z)
     x <- index(z)
     f <- water.year(x)
-    grp <- cut(y, quantile(y))
+    grp <- cut(y, quantile(y), include.lowest = T)
     cols <- brewer.pal(length(levels(grp)), palette)
     pl <- xyplot(y ~ x | f, groups = grp, panel = function(x, 
         y, ...) {
         panel.lines(x, y, col = "black")
         panel.superpose(x, y, ...)
-    }, scales = list(x = list(relation = "free"), y = list(log = F)), 
-        type = c("p"), layout = c(1, 7), as.table = T, ylim = c(-1, 
+    }, scales = list(x = list(relation = "free"), y = list(log = log)), 
+        type = c("p"), layout = c(1, nlevels(f)), as.table = T, ylim = c(-1, 
             quantile(y, probs = c(0.975))), ylab = "Discharge (cfs)", 
         xlab = "", par.settings = list(superpose.symbol = list(fill = cols, 
             col = cols, pch = 16), superpose.line = list(col = "black")), 
